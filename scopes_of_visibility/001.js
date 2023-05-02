06bf32d4c1f84458d8c13b3283d713208ad6cf33
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-02 19:42:38 +0300

    end lesson scopes of visibility

--- scopes_of_visibility/001.js
+++ scopes_of_visibility/001.js
@@ -1,13 +1,17 @@
+'use strict'
+
 let a
 let b
 
 function myFn() {
     let b
+    let a
     a = true
     b = 10
-    console.log(b)
+    console.log(a)
 }
 
 myFn()
+
 console.log(a)
 console.log(b)
