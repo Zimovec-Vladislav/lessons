43a761a77a23e21edd13d2e63e398b6e84c5b417
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-12 20:37:34 +0300

    method map

--- arrays/main.js
+++ arrays/main.js
@@ -1,8 +1,11 @@
 const myArray = [1, 2, 3, 11]
 console.log(myArray)
 
-const res = myArray.forEach(el => console.log(el*2))
+const newArray = myArray.map(function (el) {
+    if (el > 2) {
+        return el*10
+    } else return el
+})
 
-console.log(res)
-
+console.log(newArray)
 console.log(myArray)
