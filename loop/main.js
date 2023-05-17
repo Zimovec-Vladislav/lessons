cc2ec67f60639d31012625c169b0933b2e730da6
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-17 19:32:47 +0300

    loop for in

--- loop/main.js
+++ loop/main.js
@@ -1,6 +1,9 @@
-let i = 10
-
-do {
-    console.log(i)
-    i++
-} while (i < 5)
+const myObject = {
+    x: 10,
+    y: true,
+    z: 'abc'
+}
+let i = 0
+for (const key in myObject) {
+    console.log(key, myObject[key])
+}
