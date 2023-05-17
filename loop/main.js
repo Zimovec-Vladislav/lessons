260dfdfdb7c90a6ecc5da35cc30389aef6d201b5
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-17 20:06:27 +0300

    methods keys and values for objects

--- loop/main.js
+++ loop/main.js
@@ -3,7 +3,15 @@ const myObject = {
     y: true,
     z: 'abc'
 }
-let i = 0
+
 for (const key in myObject) {
     console.log(key, myObject[key])
 }
+
+Object.keys(myObject).forEach(key => {
+    console.log(key, myObject[key])
+})
+
+Object.values(myObject).forEach(value => {
+    console.log(value)
+})
