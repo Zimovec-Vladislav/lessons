860fa2e030c5f342d9819992905b94b851e25148
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-20 18:37:34 +0300

    classes methods

---
+++ classes/main.js
@@ -0,0 +1,20 @@
+class Comment {
+    constructor(text) {
+        this.text = text
+        this.votesQty = 0
+    }
+
+    upvote() {
+        this.votesQty += 1
+    }
+}
+
+const firstComment = new Comment('First comment')
+
+firstComment instanceof Comment
+firstComment instanceof Object
+
+firstComment.upvote()
+console.log(firstComment.votesQty)
+firstComment.upvote()
+console.log(firstComment.votesQty)
