ea495c686934d023e4f2d55ba11800d91a290627
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-22 22:14:01 +0300

    static method

--- classes/main.js
+++ classes/main.js
@@ -1,20 +1,11 @@
 class Comment {
-    constructor(text) {
+    constructor (text) {
         this.text = text
-        this.votesQty = 0
     }
 
-    upvote() {
-        this.votesQty += 1
+    static mergeCommentary (first, second) {
+        return `${first}, ${second}`
     }
 }
 
-const firstComment = new Comment('First comment')
-
-firstComment instanceof Comment
-firstComment instanceof Object
-
-firstComment.upvote()
-console.log(firstComment.votesQty)
-firstComment.upvote()
-console.log(firstComment.votesQty)
+Comment.mergeCommentary('The blue whale', 'infinite ocean')
