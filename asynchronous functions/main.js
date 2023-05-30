4d10de94e849c47f2bcd946a120fbe5cc209009a
Zimovec-Vladislav <boss.zimovet@gmail.com>
2023-05-30 20:02:24 +0300

    end lesson async/await

--- asynchronous functions/main.js
+++ asynchronous functions/main.js
@@ -1,13 +1,28 @@
-const timerFunction = () =>
-    new Promise ((resolve, reject) => 
-    setTimeout(() => resolve(), 1000))
+// const getData = (url) =>
+//     new Promise((resolve, reject) =>
+//         fetch(url)
+//             .then(response => response.json())
+//             .then(json => resolve(json))
+//             .catch(error => reject(error))
+//         )
 
-const asyncFn = async () => {
-    console.log('Timer starts')
-    const timeStart = performance.now()
-    await timerFunction()
-    const timeEnd = performance.now()
-    console.log('Timer ended', timeEnd - timeStart)
+const getData = async (url) => {
+    const res = await fetch(url)
+    const json = await res.json()
+    return json
 }
 
-asyncFn()
+// getData('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error.message))
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+// const data = await getData(url)
+
+try {
+    const data = await getData(url)
+    console.log(data)
+} catch (error) {
+    console.log(error.message)
+}
